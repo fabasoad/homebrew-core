fb37d67c7b6eb8693c9e6207127ee043751c9b5c
Yevhen Fabizhevskyi <yevhen.fabizhevskyi@tri-ad.global>
2020-07-04 12:40:11 +0900

    Some additional changes

--- Formula/ghacu.rb
+++ Formula/ghacu.rb
@@ -1,13 +1,17 @@
 class Ghacu < Formula
-    desc "GitHub Actions Check Updates - CLI tool to check whether all your actions are up-to-date or not."
-    homepage ""
-    url "https://github.com/fabasoad/ghacu/releases/download/v1.1.4/ghacu-1.1.4-osx-x64.tgz"
-    sha256 "8d26db0e6137b2153803fe93a8f4f099644057da3748fc6f3aebe866c0f3b8c7"
+    desc "GitHub Actions Check Updates - CLI tool to check whether all your actions are up-to-date or not"
+    homepage "https://github.com/fabasoad/ghacu"
+    url "https://github.com/fabasoad/ghacu/archive/v1.1.4.tar.gz"
+    sha256 "55b195b8cbeaab926e37fe86c92593681172c66935b0df898a4d62aaac74c8c8"
     license "https://raw.githubusercontent.com/fabasoad/ghacu/master/LICENSE"
+    version "1.1.4"
+
+    depends_on :powershell
   
     bottle :unneeded
   
     def install
+        bin.install "./build.ps1", "-os", "macos-latest"
     end
   
     test do
